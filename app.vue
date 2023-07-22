6b00af49c6d5cfbca3fc6ac3fd74c357388afbde
Руслан Мельник <melnk300@gmail.com>
2023-07-22 10:28:20 +0500

    Initial

---
+++ app.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <NuxtPage/>
+  </div>
+</template>
+
+<style lang="less">
+  :root {
+    --primary: #333;
+    --text: #f1f1f1;
+    --accent: #00C155;
+    --lighter: #777;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    background: var(--primary);
+    color: var(--text);
+  }
+
+  @font-face {
+    font-family: "Milky Mania";
+    src: local("Milky Mania"),
+    url(~assets/MilkyMania.ttf) format("truetype");
+  }
+
+
+  * {
+    box-sizing: border-box;
+    font-family: "Roboto", sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+</style>
