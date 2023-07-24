316d795d0c53df3bfe6a4d9819c4a25be8250928
Руслан Мельник <melnk300@gmail.com>
2023-07-24 16:51:10 +0500

    [refactored/episodes] Refactored main view
    WID:
    [x] Added page transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: 'slide', mode: 'out-in' }
+  },
   devtools: { enabled: true },
   modules: [
     '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt'
   ],
+  imports: {
+    dirs: ['store']
+  },
   ssr: false,
 })
